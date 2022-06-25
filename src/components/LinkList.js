75de7e3e3093dd4fded9cde426f741ec2ca9a9d7
Jenniferlala <kekvkikn@gmail.com>
2022-06-26 01:22:05 +0800

    Remove padding from list wrapper.

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -2,9 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import LinkItem from './LinkItem';
 
-const StyledList = styled.ul`
-    padding-top: 4rem;
-`;
 function LinkList({ data, handleRemoveData }) {
     const handleLinkClick = function (idx, url) {
         window.open(url, '_blank');
@@ -13,13 +10,13 @@ function LinkList({ data, handleRemoveData }) {
     };
 
     return (
-        <StyledList>
+        <ul>
             {data.links.map((link, idx) => (
                 <LinkItem key={link.id} idx={idx} url={link.url} handleLinkClick={handleLinkClick}>
                     {link.title}
                 </LinkItem>
             ))}
-        </StyledList>
+        </ul>
     );
 }
 
